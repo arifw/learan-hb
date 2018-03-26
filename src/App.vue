e647ea31eabf3764b06d8de7fbfec4a08ee91d66
arif w <arif.widiyanto@gmail.com>
2018-03-26 18:44:22 +0700

    =update to running ok

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,60 @@
 <template>
-  <v-app light>
-    <v-navigation-drawer
-      fixed
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      app
-    >
-      <v-list>
-        <v-list-tile 
-          v-for="(item, i) in items"
-          :key="i"
-          value="true"
-        >
-          <v-list-tile-action>
-            <v-icon light v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn 
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+  <v-app dark>
+    <v-toolbar dark fixed dense>
+      <v-toolbar-title>Heartbeat</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
+      <v-btn icon>
+        <v-icon>close</v-icon>
       </v-btn>
     </v-toolbar>
-    <v-content>
-      <v-container fluid>
-        <v-slide-y-transition mode="out-in">
-          <v-layout column align-center>
-            <img src="/public/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
-          </v-layout>
-        </v-slide-y-transition>
+
+    <main>
+    <!-- <v-content> -->
+      <v-container fluid class="pa-0">
+        <v-layout column>
+          <v-slide-y-transition mode="out-in">
+            <v-slide-y-transition mode="out-in">
+              <router-view></router-view>
+            </v-slide-y-transition>
+          </v-slide-y-transition>
+        </v-layout>
       </v-container>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      fixed
-      :right="right"
-      v-model="rightDrawer"
-      app
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+    <!-- </v-content> -->
+    </main>
+
+    <v-footer :fixed="true">
+      <v-btn icon>
+        <v-icon class="grey--text text-darken-3">settings</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn v-if="!$route.path.includes('/remote')" fab top right absolute class="accent" @click="add">
+        <v-icon>add</v-icon>
+      </v-btn>
     </v-footer>
+
   </v-app>
 </template>
 
 <script>
+  import HbRemotesList from './components/RemotesList.vue'
   export default {
+    components: {
+      HbRemotesList
+    },
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'bubble_chart', title: 'Inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+      }
+    },
+    methods: {
+      add () {
+        this.$router.push('/remote')
       }
     }
   }
 </script>
+
+<style>
+main {
+  margin-top: 40px;
+}
+</style>
